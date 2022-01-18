ffb286c71adf651535e13c8b2c6f0e3ecc8c62a6
Mathieu Tourrette <mathieu.tourrette@epita.fr>
2022-01-18 20:40:40 +0100

    change graph

--- theorique/graph.py
+++ theorique/graph.py
@@ -1,17 +1,24 @@
 import sys
+from node import Node
+
 
 class Graph():
 
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)] 
-                    for row in range(vertices)]
+    def __init__(self, nodes):
+        self.node = {}
+        for i in nodes:
+            self.nodes[i] = {}
 
-    def pSol(self, dist):
-        print("Distance of vertex from source")
-        for node in range(self.V):
-            print(node, "t", dist[node])
- 
+    def add_weighted_edges_from(self, list_edges):
+        for e in list_edges:
+            #e = (n1,n2,w)
+            # If the edge was already there, just increase its number
+            if e.n1 in self.nodes[e.n1]:
+                self.nodes[e.n1][e.n2]["number"] += 1
+            else:
+                self.nodes[e.n1][e.n2] = {"weight": e[2], "number": 1}
+
+            self.nodes[e[1]][e[0]] = self.nodes[e[0]][e[1]]
 
     def minDistance(self, dist, sptSet):
         min = sys.maxsize
@@ -20,7 +27,6 @@ class Graph():
                 min = dist[v]
                 min_index = v
         return min_index
- 
 
     def dijkstra(self, src):
         dist = [sys.maxsize] * self.V
@@ -31,6 +37,9 @@ class Graph():
             sptSet[x] = True
             for y in range(self.V):
                 if self.graph[x][y] > 0 and sptSet[y] == False and \
-                dist[y] > dist[x] + self.graph[x][y]:
-                        dist[y] = dist[x] + self.graph[x][y]
+                        dist[y] > dist[x] + self.graph[x][y]:
+                    dist[y] = dist[x] + self.graph[x][y]
         self.pSol(dist)
+
+    def find_odd_nodes(self):
+        return [node for node in self.graph.nodes if len(self.graph[node]) % 2]
---
+++ theorique/node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, n1, n2, w):
+        self.node1 = n1
+        self.node2 = n2
+        self.weight = w
