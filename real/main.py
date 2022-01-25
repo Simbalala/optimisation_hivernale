9f2e73896b1260399638b59c18596f689db67f71
Mathieu Tourrette <mathieu.tourrette@epita.fr>
2022-01-25 16:10:35 +0100

    readme

--- real/main.py
+++ real/main.py
@@ -1,8 +1,13 @@
 from graph import Graph
 from chinese_postman import *
 import osmnx as ox
+import argparse
 
 def main():
+    args = parse_args()
+
+
+
     g_osmnx = ox.graph_from_place('Anjou, Montréal, QC, Canada', network_type='drive')
     G = Graph(g_osmnx.nodes)
     G.add_edges(g_osmnx.edges(data='length'))
@@ -16,6 +21,9 @@ def main():
     print("Distance total parcourue in km: ", tpk)
     print(f"Ratio: {(ttk / tpk)* 100}%")
     print("\nResult route path:", cycle)
+
+
+
 
 if __name__ == "__main__":
     main()
